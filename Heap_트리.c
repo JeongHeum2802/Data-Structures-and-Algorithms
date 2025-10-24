5ec001d401054145cb9440ff53e87ecbfcf709bf
JeongHeum <wjdgma60@gmail.com>
2025-10-24 15:35:06 +0900

    Update Heap_트리.c

--- Heap_트리.c
+++ Heap_트리.c
@@ -2,219 +2,200 @@
 #include <stdlib.h>
 
 typedef struct Node {
+	struct Node* parent;
 	struct Node* left;
 	struct Node* right;
-	struct Node* parent;
-	int key;
+	int k;
 } Node;
 
-Node* getNode(); // 비어있는 노드 반환
-int isExternal(Node* v); // 외부 노드인지 확인
-Node* advanceLast(Node* v); // last노드 전진
-Node* retreatLast(Node* v); // last노드 후퇴
-void insertItem(Node** root, Node** lastNode, int key); // 아이텀 추가
-int deleteItem(Node** root, Node** lastNode); // 아이템 제거 후 반환
-void expandExternal(Node* v); // 외부노드 확장
-void reduceExternal(Node** root, Node* v); // 노드 제거 후 외부노드 축소
-void upHeap(Node* v); // upHeap
-void downHeap(Node* v); // downHeap
-
+// util
+Node* getNode();
+int isExternal(Node* v);
+void swapEle(Node* a, Node* b);
 void printHeap(Node* root);
-void freeHeap(Node* root);
+void freeHeap(Node* v);
+
+// LastNode
+Node* advanceLast(Node* root, Node* last);
+Node* retreatLast(Node* root, Node* last);
+
+// insert
+void insertItem(Node* root, Node** lastNode, int k);
+void expandExternal(Node* v);
+void upHeap(Node* v);
+
+// remove
+int removeItem(Node** root, Node** lastNode);
+void reduceExternal(Node** root, Node* v);
+void downHeap(Node* v);
+
 
 int main() {
 	Node* root = getNode();
 	Node* lastNode = NULL;
 
-	char cmd;
-	scanf("%c", &cmd);
-	while (cmd != 'q') {
-		if (cmd == 'i') {
-			int key;
-			scanf("%d", &key);
-			insertItem(&root, &lastNode, key);
-		} 
-		else if (cmd == 'd') {
-			int key = deleteItem(&root, &lastNode);
-			printf("%d\n", key);
-		}
-		else if (cmd == 'p') {
-			printHeap(root);
-			printf("\n");
-		}
+	insertItem(root, &lastNode, 3);
+	insertItem(root, &lastNode, 6);
+	insertItem(root, &lastNode, 2);
+	insertItem(root, &lastNode, 9);
 
-		scanf(" %c", &cmd);
-	}
+	printHeap(root);
+	printf("\n");
+
+	printf("%d\n", removeItem(&root, &lastNode));
+	printf("%d\n", removeItem(&root, &lastNode));
+	printf("%d\n", removeItem(&root, &lastNode));
+	printf("%d\n", removeItem(&root, &lastNode));
 
 	freeHeap(root);
 	return 0;
 }
 
-
-void printHeap(Node* root) {
-	if (isExternal(root)) return;
-	printf("%d ", root->key);
-	printHeap(root->left);
-	printHeap(root->right);
-}
-void freeHeap(Node* root) {
-	if (root == NULL) return;
-	freeHeap(root->left);
-	freeHeap(root->right);
-
-	free(root);
-}
-
-// 노드 반환
+//// util
 Node* getNode() {
 	Node* newNode = (Node*)malloc(sizeof(Node));
 	newNode->left = NULL;
 	newNode->right = NULL;
 	newNode->parent = NULL;
-	newNode->key = -1;
+	newNode->k = -1;
 	return newNode;
 }
-
-// 외부 노드면 1 아니면 0 리턴
 int isExternal(Node* v) {
-	return (v->left == NULL && v->right == NULL);
+	return v->left == NULL && v->right == NULL;
+}
+void swapEle(Node* a, Node* b) {
+	int temp = a->k;
+	a->k = b->k;
+	b->k = temp;
+}
+void printHeap(Node* root) {
+	if (isExternal(root)) return;
+	printf(" %d", root->k);
+	printHeap(root->left);
+	printHeap(root->right);
+}
+void freeHeap(Node* v) {
+	if (v == NULL) return;
+	freeHeap(v->left);
+	freeHeap(v->right);
+	free(v);
 }
 
-// lastNode를 다음 위치로 옮김
-Node* advanceLast(Node* v) {
+//// LastNode
 
-	// 자신이 오른쪽 자식인 동안 부모로
-	Node* p = v->parent;
-	while (p != NULL && p->right == v) {
-		v = p;
-		p = v->parent;
-	}
+// 새로 삽입될 노드를 반환
+Node* advanceLast(Node* root, Node* last) {
+	if (isExternal(root)) // 힙이 비어있으면 루트노드 반환
+		return root;
 
-	// 자신이 왼쪽 자식이면 형제로
-	if (p != NULL && p->left == v)
-		v = p->right;
+	Node* v = last;
 
+	// 루트가 아니고 부모의 오른쪽에 위치할 때
+	while (v->parent && v->parent->right == v) 
+		v = v->parent;
+
+	if (v->parent && v->parent->left == v) // 형제로 이동
+		v = v->parent->right;
+
+	// 왼쪽 외부노드로 이동
 	while (!isExternal(v))
 		v = v->left;
 
 	return v;
 }
+// 후퇴될 노드를 반환
+Node* retreatLast(Node* root, Node* last) {
+	if (last == root) // 원소가 하나인 경우
+		return NULL;
 
-// lastNode를 전 위치로 옮김 (원소가 없으면 NULL 리턴)
-Node* retreatLast(Node* v) {
+	Node* v = last;
 	
-	// 자신이 왼쪽 자식인 경우 부모로
-	Node* p = v->parent;
-	while (p != NULL && p->left == v) {
-		v = p;
-		p = v->parent;
-	}
+	// 자신이 부모의 왼쪽 자식일 때
+	while (v->parent && v->parent->left == v)
+		v = v->parent;
 
-	if (p != NULL && p->right == v)
-		v = p->left;
+	// 형제로 가기
+	if (v->parent && v->parent->right == v)
+		v = v->parent->left;
 
-	while (!isExternal(v))
+	while (!isExternal(v->right))
 		v = v->right;
 
-	return v->parent;
+	return v;
 }
 
-// 힙에 아이템 삽입
-void insertItem(Node** root, Node** lastNode, int key) {
-
-	// 아이템이 없으면 루트 변경
-	if (isExternal(*root)) {
-		*lastNode = *root;
-	}
-	else {
-		*lastNode = advanceLast(*lastNode);
-	}
-
-	(*lastNode)->key = key;
+//// insert
+void insertItem(Node* root, Node** lastNode, int k) {
+	*lastNode = advanceLast(root, *lastNode);
+	(*lastNode)->k = k;
 	expandExternal(*lastNode);
 	upHeap(*lastNode);
-	return;
 }
-
-// 힙에서 아이템을 제거하고 값을 반환
-int deleteItem(Node** root, Node** lastNode) {
-	if (isExternal(*root)) return -404;
-
-	int key = (*root)->key;
-
-	(*root)->key = (*lastNode)->key; // 루트에 마지막 노드 값 대입
-
-	Node* v = *lastNode;
-	Node* retreatNode = retreatLast(*lastNode);
-	if (*lastNode == retreatNode) { // 원소가 하나 남았을 때 lastNode 를 NULL
-		*lastNode = NULL;
-	}
-	else {
-		*lastNode = retreatNode;
-	}
-
-	Node* z = v->left;
-	reduceExternal(root, z);
-	downHeap(*root);
-
-	return key;
-}
-
-// 노드에 외부노드 연장
 void expandExternal(Node* v) {
 	Node* left = getNode();
 	Node* right = getNode();
 	left->parent = v;
 	right->parent = v;
+
 	v->left = left;
 	v->right = right;
-	return;
 }
-
-// 노드 축소
-void reduceExternal(Node** root, Node* z) {
-	Node* y = z->parent;
-	Node* g = y->parent;
-	Node* zs = (y->left == z) ? y->right : y->left;
-
-	zs->parent = g;
-	if (zs->parent == NULL) *root = zs;
-	else {
-		if (g->left == y) g->left = zs;
-		else g->right = zs;
-	}
-
-	free(y);
-	free(z);
-}
-
-// upHeap
 void upHeap(Node* v) {
-	// 루트, 값이 부모보다 크거나 같으면 return
-	if (v->parent == NULL || v->key >= v->parent->key) return;
+	if (v->parent == NULL) return;
 
-	// upHeap
-	int temp = v->key;
-	v->key = v->parent->key;
-	v->parent->key = temp;
+	if (v->parent->k <= v->k) return;
+
+	swapEle(v->parent, v);
 	upHeap(v->parent);
-
-	return;
 }
 
-// downHeap
-void downHeap(Node* v) {
-	if (isExternal(v) || isExternal(v->left)) return; // 원소가 한개일 때 외부노드v로 접근됨
+//// remove
+int removeItem(Node** root, Node** lastNode) {
+	int ele = (*root)->k;
+	swapEle(*root, *lastNode);
 
-	Node* smaller = v->left;
-	if (!isExternal(v->right) && smaller->key > v->right->key) {
-		smaller = v->right;
+	// lastNode 후퇴 후 축소
+	Node* z = *lastNode;
+	*lastNode = retreatLast(*root, *lastNode);
+	reduceExternal(root, z->left);
+
+	// downHeap
+	downHeap(*root);
+	return ele;
+}
+void reduceExternal(Node** root, Node* v) {
+	Node* w = v->parent; // 부모
+	Node* g = w->parent; // 조상
+	Node* zs = (w->left == v) ? w->right : w->left; // 형제
+
+	if (g == NULL) { // 조상이 없음 (원래 w가 루트)
+		*root = zs;
+		zs->parent = NULL;
+	}
+	else {
+		if (g->right == w)
+			g->right = zs;
+		else
+			g->left = zs;
+		zs->parent = g;
 	}
 
-	if (v->key <= smaller->key) return;
-	int temp = v->key;
-	v->key = smaller->key;
-	smaller->key = temp;
+	free(v);
+	free(w);
+}
+void downHeap(Node* v) {
+	if (isExternal(v)) return;
+	Node* left = v->left;
+	Node* right = v->right;
 
+	Node* smaller = NULL;
+	if (!isExternal(left))
+		smaller = left;
+	if (!isExternal(right) && right->k < smaller->k)
+		smaller = right;
+
+	if (smaller == NULL || v->k <= smaller->k) return;
+
+	swapEle(v, smaller);
 	downHeap(smaller);
 }
